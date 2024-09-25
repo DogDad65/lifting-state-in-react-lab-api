86bad6cd3dc6d55b2bfaf91a6dbd1567af0fac0a
Banton65 <banton65@gmail.com>
2024-09-25 18:22:45 -0400

    lifting-lab-react

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,20 @@
+// `src/components/BurgerStack.jsx`
+const BurgerStack = ({stack, removeFromBurger}) => {
+    return (
+        <ul>
+            {stack.length === 0 ? (
+                <li>No Ingredients</li>
+            ) : (
+                stack.map((ingredient, index) => (
+                    <li key={index} style={{ backgroundColor: ingredient.color}}>
+                        {ingredient.name}
+                        <button onClick={() => removeFromBurger(index)}>X</button>
+                    </li>
+                ))
+            )}        
+        </ul>
+    )
+  };
+  
+  export default BurgerStack;
+  
